66bad4b0b466a13b3e61f12cb686295dfa26cbac
Fernandes072 <joaofilho8274@gmail.com>
2023-11-27 10:01:31 -0300

    Find by username, email and registration or name return User

--- src/main/java/com/ifs/requerimento/repositories/UserRepository.java
+++ src/main/java/com/ifs/requerimento/repositories/UserRepository.java
@@ -6,16 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ifs.requerimento.entities.User;
-import com.ifs.requerimento.projections.UserProjection;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM users WHERE username = :username")
-	UserProjection findByUsername(String username);
+	User findByUsername(String username);
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM users WHERE email = :email")
-	UserProjection findByEmail(String email);
+	User findByEmail(String email);
 	
 	@Query(nativeQuery = true, value = "SELECT * FROM users WHERE LOWER(registration) LIKE LOWER(CONCAT('%', :registrationName, '%')) OR LOWER(name) LIKE LOWER(CONCAT('%', :registrationName, '%'))")
-	List<UserProjection> findByRegistrationOrName(String registrationName);
+	List<User> findByRegistrationOrName(String registrationName);
 }
